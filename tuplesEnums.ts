c991c73711357b897517cf4c9976cbf79592eabf
BillStephens2022 <two4onebill@yahoo.com>
2023-08-24 15:28:41 -0400

    added enum examples

--- tuplesEnums.ts
+++ tuplesEnums.ts
@@ -13,3 +13,27 @@ const badRes: HTTPResponse = [404, "Not Found"];
 
 const responses: HTTPResponse[] = [[200, "OK"],[404, "Not Found"]];
 
+// Enums
+enum OrderStatus {   // assigns a default value (i.e. 0, 1, 2, 3 below.  You can change this by setting each order status to a different number i.e. DELIVERED = 43)
+    PENDING,   //OrderStatus.PENDING = 0
+    SHIPPED,  //OrderStatus.SHIPPED = 1
+    DELIVERED,  //OrderStatus.DELIVERED = 2
+    RETURNED  //OrderStatus.RETURNED = 3
+}
+
+const myStatus = OrderStatus.DELIVERED;
+
+function isDelivered(status: OrderStatus) {
+    return status === OrderStatus.DELIVERED;
+}
+
+isDelivered(OrderStatus.RETURNED);
+
+// instead of using the default values (0, 1, 2, 3), you can set each ArrowKey to a string instead....
+enum ArrowKeys {
+    UP = "up",
+    DOWN = "down",
+    LEFT = "left",
+    RIGHT = "right"
+}
+
